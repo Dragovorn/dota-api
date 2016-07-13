d6f4c4126d370d39d83b677e5ed44177942dde49
Dragovorn <andrewburr011@gmail.com>
2016-07-12 21:30:27 -0400

    First Commit

---
+++ src/main/java/com/dragovorn/dotaapi/IDotaAPI.java
@@ -0,0 +1,11 @@
+package com.dragovorn.dotaapi;
+
+/**
+ * *************************************************************************
+ * (c) Dragovorn 2016. This file was created by Andrew at 09:26 PM.
+ * as of July 12 2016 the project DotaAPI is Copyrighted.
+ * *************************************************************************
+ */
+public interface IDotaAPI {
+
+}
